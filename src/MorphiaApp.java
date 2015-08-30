4ed1b2ccca0a6abb00ed74acdb2173768d0f406f
Merzak <merzak17@gmail.com>
2015-08-30 09:17:23 +0100

    Modify MorphiaApp.java

--- src/MorphiaApp.java
+++ src/MorphiaApp.java
@@ -1,26 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import com.abc.Address;
+import com.abc.Person;
+import com.mongodb.*;
+import com.mongodb.MapReduceCommand.OutputType;
+import com.mongodb.client.MongoIterable;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Key;
-import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.UpdateOperations;
 import org.mongodb.morphia.query.UpdateResults;
 
-import com.abc.Address;
-import com.abc.Person;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.MapReduceCommand;
-import com.mongodb.MapReduceCommand.OutputType;
-import com.mongodb.MapReduceOutput;
-import com.mongodb.MongoClient;
-import com.mongodb.ServerAddress;
-import com.mongodb.WriteResult;
-import com.mongodb.client.MongoIterable;
+import java.util.List;
 
 public class MorphiaApp {
+
+	private static ConnectDB connectDB;
+	private Datastore datastore;
 
 	public static void main(String[] args) {
 		final MorphiaApp app = new MorphiaApp();
@@ -30,41 +24,29 @@ public class MorphiaApp {
 		app.delete();
 		app.list();
 		app.mapreduce();
+		connectDB.closeDb();
 	}
-
-	private final String mDB;
-	private final List<ServerAddress> mServers;
-	private final Morphia morphia;
 	
 	private MorphiaApp() {
-		mDB = "morphiaDB";
-		mServers = new ArrayList<ServerAddress>(3);
-		mServers.add(new ServerAddress("localhost", 28001));
-		mServers.add(new ServerAddress("localhost", 28002));
-		mServers.add(new ServerAddress("localhost", 28003));
-		
-		morphia = new Morphia();
-		morphia.mapPackage("com.abc");
+		connectDB=new ConnectDB();
+		datastore = ConnectDB.getDatastore();
 	}
 	
 	private void clean() {
-		final MongoClient mongoClient = new MongoClient(mServers);
 
-		final MongoIterable<String> dbs = mongoClient.listDatabaseNames();
+		final MongoIterable<String> dbs = connectDB.getDBNames();
 
 		for (String db : dbs) {
 			System.out.println(db);
 		}
 		
-		mongoClient.dropDatabase(mDB);		
+		connectDB.dropDB();
 		
-		mongoClient.close();
+		connectDB.closeDb();
 	}
 	
 	private void create() {
-		final MongoClient mongoClient = new MongoClient(mServers);
 
-		final Datastore datastore = morphia.createDatastore(mongoClient, mDB);
 		datastore.ensureIndexes();
 
 		for (int i = 0; i < 10; i++) {
@@ -99,14 +81,10 @@ public class MorphiaApp {
 			System.out.println("Created " + pkey);
 		}
 
-		mongoClient.close();
 	}
 	
 	private void update() {
-		final MongoClient mongoClient = new MongoClient(mServers);
 
-		final Datastore datastore = morphia.createDatastore(mongoClient, mDB);
-				
 		final UpdateOperations<Person> updateOps = datastore.createUpdateOperations(Person.class)
 							.set("lastName", "Smith").set("sex", "male");
 		
@@ -117,43 +95,31 @@ public class MorphiaApp {
 		query = datastore.createQuery(Person.class).field("lastName").equal("Smith 8");
 		final UpdateResults ret = datastore.update(query, updateOps);
 		System.out.println("Updated: " + ret.getUpdatedCount());
-		
-		mongoClient.close();
+
 	}
 	
 	private void delete() {
-		final MongoClient mongoClient = new MongoClient(mServers);
 
-		final Datastore datastore = morphia.createDatastore(mongoClient, mDB);
-		
 		final Query<Person> query = datastore.createQuery(Person.class).field("lastName").equal("Smith 7");
 		final WriteResult ret = datastore.delete(query);
 		
 		System.out.println("Removed: " + ret.getN());
-		
-		mongoClient.close();
+
 	}
 	
 	private void list(){
-		final MongoClient mongoClient = new MongoClient(mServers);
 
-		final Datastore datastore = morphia.createDatastore(mongoClient, mDB);
-		
 		final Query<Person> query = datastore.createQuery(Person.class);
 		final List<Person> persons = query.asList();
 		
 		for(Person p : persons) {
 			System.out.println("Person: " + p);
 		}
-		
-		mongoClient.close();
+
 	}
 	
 	private void mapreduce(){
-		final MongoClient mongoClient = new MongoClient(mServers);
 
-		final Datastore datastore = morphia.createDatastore(mongoClient, mDB);
-		
 		final String map = "function(){emit(this.lastName, 1);}";
 		final String reduce = "function(key, values){return Array.sum(values)},{query:{lastName:\"Smith\"}, out:\"person\"}";
 		
@@ -167,7 +133,6 @@ public class MorphiaApp {
 		for(DBObject o : out.results()){
 			System.out.println(o.toString());  
 		}
-		
-		mongoClient.close();
+
 	}
 }
